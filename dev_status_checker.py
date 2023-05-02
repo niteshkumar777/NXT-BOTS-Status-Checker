f9faad81d2143a7b3557b954113e628584f5d7d3
Dev Arora <116826965+DevArora2604@users.noreply.github.com>
2023-05-02 22:41:17 +0530

    Update dev_status_checker.py

--- dev_status_checker.py
+++ dev_status_checker.py
@@ -8,6 +8,7 @@ import datetime
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 
+from config import *
 
 app = Client(
     name = "devbotz",
@@ -15,13 +16,6 @@ app = Client(
     api_hash = os.environ["API_HASH"],
     session_string = os.environ["SESSION_STRING"]
 )
-
-TIME_ZONE = os.environ["TIME_ZONE"]
-BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(' ')]
-CHANNEL_ID = int(os.environ["CHANNEL_ID"]) #CHANNEL_ID is for group/channel where checker will update the status.
-MESSAGE_ID = int(os.environ["MESSAGE_ID"])
-BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
-GRP_ID = os.environ.get("GRP_ID") #GRP_ID is for logs group where checker will send warnings of offline bots.
 
 async def main_devchecker():
     async with app:
