46bc514aa984e61e4f79fa898a4a75f511dedb99
Dev Arora <116826965+DevArora2604@users.noreply.github.com>
2023-05-02 22:40:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
 
+
+TIME_ZONE = os.environ["TIME_ZONE"]
+BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(' ')]
+CHANNEL_ID = int(os.environ["CHANNEL_ID"]) #CHANNEL_ID is for group/channel where checker will update the status.
+MESSAGE_ID = int(os.environ["MESSAGE_ID"])
+BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
+GRP_ID = os.environ.get("GRP_ID") #GRP_ID is for logs group where checker will send warnings of offline bots.
