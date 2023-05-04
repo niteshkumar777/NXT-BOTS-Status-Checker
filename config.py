6e99569832616c008f159341eabf2bcdb9a35fef
Dev Arora <116826965+DevArora2604@users.noreply.github.com>
2023-05-04 21:00:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 TIME_ZONE = getenv("TIME_ZONE")
-BOT_LIST = 
-CHANNEL_ID = 
-MESSAGE_ID = 
-BOT_ADMIN_IDS = 
-GRP_ID = 
+BOT_LIST = list(getenv("BOT_LIST").split())
+CHANNEL_ID = getenv("CHANNEL_ID")
+MESSAGE_ID = int(getenv("MESSAGE_ID"))
+BOT_ADMIN_IDS = list(getenv("BOT_ADMIN_IDS"))
+GRP_ID = getenv("GRP_ID")
