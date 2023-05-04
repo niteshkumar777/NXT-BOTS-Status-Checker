91d30fd3bfaa2ef2ca73ef341b95bbad5d388df4
Dev Arora <116826965+DevArora2604@users.noreply.github.com>
2023-05-04 21:06:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-TIME_ZONE = getenv("TIME_ZONE")
+API_ID = int(getenv("API_ID", "27353035"))
+API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
+TIME_ZONE = getenv("TIME_ZONE", "Aisa/Kolkata")
 BOT_LIST = list(getenv("BOT_LIST").split())
 CHANNEL_ID = getenv("CHANNEL_ID")
 MESSAGE_ID = int(getenv("MESSAGE_ID"))
 BOT_ADMIN_IDS = list(getenv("BOT_ADMIN_IDS"))
 GRP_ID = getenv("GRP_ID")
+SESSION = getenv("SESSION")
